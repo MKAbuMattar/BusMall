f5603d09ef63853ccf356877da8bbb6109beeb42
MKAbuMattar <mohammad.khaled@outlook.com>
2021-03-29 13:55:17 +0300

    chartJS

--- js/app.js
+++ js/app.js
@@ -28,9 +28,11 @@ const leftImg = document.getElementById('left-img');
 const centerImg = document.getElementById('center-img');
 const rightImg = document.getElementById('right-img');
 
-const btnScore = document.getElementById('btn-score');
+// const btnScore = document.getElementById('btn-score');
 
-const productList = document.getElementById('product-list');
+// const productList = document.getElementById('product-list');
+
+let ctx = document.getElementById('myChart').getContext('2d');
 
 let leftIndex;
 let centerIndex;
@@ -38,6 +40,9 @@ let rightIndex;
 
 let numVote = 25;
 let totnumVote = 0;
+
+let views = [];
+let votes = [];
 
 function randomNumber(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -57,12 +62,23 @@ for (let i = 0; i < names.length; i++) {
 }
 
 function render() {
-  leftIndex = randomNumber(0, Product.all.length - 1);
-  centerIndex = randomNumber(0, Product.all.length - 1);
-  rightIndex = randomNumber(0, Product.all.length - 1);
+  leftIndex = Number(randomNumber(0, Product.all.length - 1));
+  centerIndex = Number(randomNumber(0, Product.all.length - 1));
+  rightIndex = Number(randomNumber(0, Product.all.length - 1));
 
-  if (leftIndex === centerIndex || leftIndex === rightIndex || centerIndex === rightIndex) {
+  // console.log(`leftIndex = ${leftIndex}\ncenterIndex = ${centerIndex}\nrightIndex = ${rightIndex}\n`);
+
+  let tempLeftIndex;
+  let tempCenterIndex;
+  let tempRightIndex;
+
+
+  if ((leftIndex === centerIndex || centerIndex === rightIndex || leftIndex === rightIndex) ||
+    tempLeftIndex === leftIndex || tempLeftIndex === centerIndex || tempLeftIndex === rightIndex ||
+    tempRightIndex === leftIndex || tempRightIndex === centerIndex || tempRightIndex === rightIndex ||
+    tempCenterIndex === leftIndex || tempCenterIndex === centerIndex || tempCenterIndex === rightIndex) {
     render();
+    // console.log(`tempLeftIndex = ${tempLeftIndex}\ntempCenterIndex = ${tempCenterIndex}\ntempRightIndex = ${tempRightIndex}\n`);
   }
   else {
     leftImg.src = Product.all[leftIndex].path;
@@ -79,6 +95,12 @@ function render() {
     rightImg.alt = Product.all[rightIndex].name;
     rightImg.title = Product.all[rightIndex].name;
     Product.all[rightIndex].views++;
+
+    tempLeftIndex = leftIndex;
+    tempCenterIndex = centerIndex;
+    tempRightIndex = rightIndex;
+
+    // console.log(`tempLeftIndex = ${tempLeftIndex}\ntempCenterIndex = ${tempCenterIndex}\ntempRightIndex = ${tempRightIndex}\n`);
   }
 }
 
@@ -100,30 +122,67 @@ function swapImg(event) {
     totnumVote++;
     render();
   } else {
-    btnScore.classList.remove('hide');
+    // btnScore.classList.remove('hide');
+
+    for (let i = 0; i < names.length; i++) {
+      views.push(Product.all[i].views);
+      votes.push(Product.all[i].votes);
+    }
+
+    chartJS();
     imgSection.removeEventListener('click', swapImg);
   }
 }
 
-btnScore.addEventListener('click', showProductList);
+// btnScore.addEventListener('click', showProductList);
 
-function showProductList() {
+// function showProductList() {
 
-  const h2El = document.createElement('h2');
-  productList.appendChild(h2El);
-  h2El.setAttribute('class', 'row center');
-  h2El.textContent = 'Product List';
+//   const h2El = document.createElement('h2');
+//   productList.appendChild(h2El);
+//   h2El.setAttribute('class', 'row center');
+//   h2El.textContent = 'Product List';
 
-  const ulEl = document.createElement('ul');
-  productList.appendChild(ulEl);
+//   const ulEl = document.createElement('ul');
+//   productList.appendChild(ulEl);
 
-  for (let i = 0; i < names.length; i++) {
-    const liEl = document.createElement('li');
-    ulEl.appendChild(liEl);
-    liEl.textContent = `${Product.all[i].name} had ${Product.all[i].votes} votes, and was seen ${Product.all[i].views} times.`;
-  }
+//   for (let i = 0; i < names.length; i++) {
+//     const liEl = document.createElement('li');
+//     ulEl.appendChild(liEl);
+//     liEl.textContent = `${Product.all[i].name} had ${Product.all[i].votes} votes, and was seen ${Product.all[i].views} times.`;
 
-  btnScore.removeEventListener('click', showProductList);
+//     views.push(Product.all[i].views);
+//     votes.push(Product.all[i].votes);
+//   }
+
+//   btnScore.removeEventListener('click', showProductList);
+// }
+
+function chartJS() {
+
+  let chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'bar',
+
+    // The data for our dataset
+    data: {
+      labels: names,
+      datasets: [{
+        label: 'Product views',
+        backgroundColor: 'rgb(255, 99, 132)',
+        borderColor: 'rgb(255, 99, 132)',
+        data: views
+      }, {
+        label: 'Product votes',
+        backgroundColor: 'rgb(170, 86, 86)',
+        borderColor: 'rgb(170, 86, 86)',
+        data: votes
+      }]
+    },
+
+    // Configuration options go here
+    options: {}
+  });
 }
 
 render();
